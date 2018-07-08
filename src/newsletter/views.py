301abfd8c21954986b0da4734ca391a4fd82e71b
Pedro Tavares <ptavaresh@gmail.com>
2018-07-07 23:44:10 -0500

    try_django18 tutorial 13 forms, models templates

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    title = 'Welcome dog!!'
+    if request.user.is_authenticated():
+        title = "My title %s" %(request.user)
+    print dir(request)
+    context = {"template_title": title}
+    return render(request, "home.html", context)
---
+++ src/readme.md
@@ -0,0 +1,12 @@
+Django 1.8 commands
+
+Run these commands in src/ path
+#django shell
+python manage.py shell
+
+#run server
+python manage.py runserver
+
+#migrations
+python manage.py makemigrations
+python manage.py  migrate
